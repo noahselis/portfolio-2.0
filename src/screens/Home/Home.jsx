c7b56a9134378abaa3bc6479d07621c16fbe7234
Noah Selis <80969784+noahselis@users.noreply.github.com>
2022-12-13 01:20:59 -0700

    hero img is about shoehorned in. itll go when im not so darn tired

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,11 +1,22 @@
 import './Home.scss'
 
+//component imports
 import Navbar from '../../components/Navbar/Navbar'
+
+//svg imports
+import { ReactComponent as LogoDark } from '../../assets/logo-dark.svg'
+import { ReactComponent as LogoLight } from '../../assets/logo-light.svg'
+import { ReactComponent as HeroImg } from '../../assets/hero-img.svg'
 
 export default function Home({}) {
   return (
     <div className="homeContainer">
-      <p>home</p>
+      <div className="logoWrap">
+        <LogoDark />
+      </div>
+      <div className="picWrap">
+        <HeroImg />
+      </div>
       <div className="navWrap">
         <Navbar />
       </div>
